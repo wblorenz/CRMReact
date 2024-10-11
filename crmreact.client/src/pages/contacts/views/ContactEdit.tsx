7b1f7617cdd6474a7daa5f510073b09f6845e404
wagner britto lorenzini <wblorenz@gmail.com>
2024-10-11 15:16:13 -0300

    feat: add all fields to the contact edit

--- crmreact.client/src/pages/contacts/views/ContactEdit.tsx
+++ crmreact.client/src/pages/contacts/views/ContactEdit.tsx
@@ -7,16 +7,25 @@ export interface ContactEditProps {
 export function ContactEdit(props: ContactEditProps) {
     const [Contact, setContact] = useState<Contact | undefined>(props.Contact);
     const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [telephone, setTelephone] = useState<string>("");
+    const [account, setAccount] = useState<string>("");
     useEffect(() => {
         if (props.Contact?.name !== undefined) {
             setContact(props.Contact);
             setName(props.Contact?.name);
+            setEmail(props.Contact?.email);
+            setTelephone(props.Contact?.telephone);
+            setAccount(props.Contact?.account);
         } else {
             setName("");
+            setEmail("");
+            setTelephone("");
+            setAccount("");
             setContact(undefined);
         }
-    },[props.Contact])
-    const handleSubmit = () => { 
+    }, [props.Contact])
+    const handleSubmit = () => {
         let method = 'post';
         if (Contact?.id !== undefined) {
             method = 'put';
@@ -26,19 +35,30 @@ export function ContactEdit(props: ContactEditProps) {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
-            }, body: JSON.stringify({ Id: Contact?.id ?? "", Name: name })
+            }, body: JSON.stringify({
+                Id: Contact?.id ?? "",
+                Name: name,
+                Email: email,
+                Telephone: telephone,
+                Account: account
+            })
         }).then((e) => e.json()).then((e) => { setContact(e); props.afterUpdate(); });
     };
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setName (event.target.value);
-        if (Contact) {
-            Contact.name = event.target.value;
-        }
+    const handleChange = (event: ChangeEvent<HTMLInputElement>, set: (s: string) => void) => {
+        set(event.target.value);
     };
     return (
         <form onSubmit={e => { e.preventDefault(); handleSubmit(); }}>
-            <input name="name" value={name} onChange={handleChange}></input>
+            <label htmlFor="name">Name:</label>
+            <input name="name" value={name} onChange={(e) => handleChange(e, setName)}></input>
+            <label htmlFor="email">Email:</label>
+            <input name="email" value={email} onChange={(e) => handleChange(e, setEmail)}></input>
+            <label htmlFor="telephone">Telephone:</label>
+            <input name="telephone" value={telephone} onChange={(e) => handleChange(e, setTelephone)}></input>
+            <label htmlFor="account">Account:</label>
+            <input name="account" value={account} onChange={(e) => handleChange(e, setAccount)}></input>
+            <br/>
             <button type="submit">{Contact?.id === undefined ? 'New' : 'Update'}</button>
         </form>
     );
